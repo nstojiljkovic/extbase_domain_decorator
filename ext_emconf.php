166d8f3568f683aaf3b328cf6c6b8e6a59b93197
Jenkins <jenkins@essentialdots.com>
2018-07-24 13:02:15 +0000

    [RELEASE] Release of extbase_domain_decorator 0.9.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '0.9.1-dev',
+	'version' => '0.9.1',
 	'constraints' => 
 	array (
 	  'depends' => 
