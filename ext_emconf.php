e8918ec621d6939f326bbd007c576d66fc9e1d1f
Jenkins <jenkins@essentialdots.com>
2019-03-08 11:03:58 +0000

    [TASK] Set extbase_domain_decorator version to 0.9.4-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '0.9.3',
+	'version' => '0.9.4-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
